9b83d26b9bf8ac2e03d544b202a88edb813c23d8
cihan_ukan <cihan.ukan@gmail.com>
2020-04-10 00:06:29 +0300

    Updates

---
+++ precache-manifest.9a1b951c076e4b608391e4243de4bd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9b5bbdf4195b3a9f33dcff5dc96227",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15d25ebd33ae8544591c",
+    "url": "/monsters-rolodex/static/css/main.d765dc85.chunk.css"
+  },
+  {
+    "revision": "51bf22b59d981cdc0e14",
+    "url": "/monsters-rolodex/static/js/2.d37fba0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.d37fba0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d25ebd33ae8544591c",
+    "url": "/monsters-rolodex/static/js/main.646f1a14.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
